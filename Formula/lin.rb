6865cde47c006ce4bddf85b2d4279fe1a2ca2b1d
Paul Somers <paulsomers@gmail.com>
2026-02-10 20:27:21 +0000

    lin 0.3.2

--- Formula/lin.rb
+++ Formula/lin.rb
@@ -1,28 +1,28 @@
 class Lin < Formula
   desc "Linear CLI for humans and LLMs"
   homepage "https://github.com/shhac/lin"
-  version "0.3.1"
+  version "0.3.2"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/shhac/lin/releases/download/v0.3.1/lin-darwin-arm64.tar.gz"
-      sha256 "cfdc7f3efb6860a3457225c75bddf9fc5fbd66570bbdeb98be9f08b95bff6e14"
+      url "https://github.com/shhac/lin/releases/download/v0.3.2/lin-darwin-arm64.tar.gz"
+      sha256 "a2396df939844509c41e2431341c346779ca6b59c97ff88e307909707c593b8c"
     end
     on_intel do
-      url "https://github.com/shhac/lin/releases/download/v0.3.1/lin-darwin-x64.tar.gz"
-      sha256 "d6e9e78608ba8774c14022ae5c3c03d8107a2cc6aadd4255f12e8c6f0a1dbb30"
+      url "https://github.com/shhac/lin/releases/download/v0.3.2/lin-darwin-x64.tar.gz"
+      sha256 "7685c2f9948ba249a18a88a110da729c7b97e10d68e23db994cde9c2ee42b9cf"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/shhac/lin/releases/download/v0.3.1/lin-linux-arm64.tar.gz"
-      sha256 "e031f8006e2be73131c2429ecabb6be357562f33c2e3d5929cab80ee10353047"
+      url "https://github.com/shhac/lin/releases/download/v0.3.2/lin-linux-arm64.tar.gz"
+      sha256 "ee6bdfd4f75187737fec49584e601179c5296454da59c54cadc7621b1a75cbaf"
     end
     on_intel do
-      url "https://github.com/shhac/lin/releases/download/v0.3.1/lin-linux-x64.tar.gz"
-      sha256 "4a53f712f7a99839991856aaedf6e9575fa42f04ba8bd0f793ceb6418cc29aa6"
+      url "https://github.com/shhac/lin/releases/download/v0.3.2/lin-linux-x64.tar.gz"
+      sha256 "755b627818e76c296f2e3854a55c9e74879b7dca160d190e0b046a84067f91fb"
     end
   end
 
@@ -31,7 +31,7 @@ class Lin < Formula
   end
 
   test do
-    assert_match "0.3.1", shell_output("#{bin}/lin --version")
+    assert_match "0.3.2", shell_output("#{bin}/lin --version")
     assert_match "Linear CLI", shell_output("#{bin}/lin --help")
   end
 end
