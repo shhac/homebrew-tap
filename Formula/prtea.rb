9367b529b2efc0e5644ba2d3b797a129a5e75b1b
Paul Somers <paulsomers@gmail.com>
2026-02-15 19:45:52 +0000

    prtea 0.2.0

--- Formula/prtea.rb
+++ Formula/prtea.rb
@@ -1,28 +1,28 @@
 class Prtea < Formula
   desc "A TUI for reviewing GitHub pull requests"
   homepage "https://github.com/shhac/prtea"
-  version "0.1.1"
+  version "0.2.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/shhac/prtea/releases/download/v0.1.1/prtea-darwin-arm64.tar.gz"
-      sha256 "0d57bd44c1dc54afe7425f0e62d4a00392b67ebc8f1395611e19f47cbf7a8fd8"
+      url "https://github.com/shhac/prtea/releases/download/v0.2.0/prtea-darwin-arm64.tar.gz"
+      sha256 "a071a50b69eedaa6eeba18784afe6bd8f9f8213a3ad66a4986ed966486a74742"
     end
     on_intel do
-      url "https://github.com/shhac/prtea/releases/download/v0.1.1/prtea-darwin-amd64.tar.gz"
-      sha256 "f1872b59770f2ef51e63cc87a226485eb683ca0f7708f4ae4b5112bd755102b7"
+      url "https://github.com/shhac/prtea/releases/download/v0.2.0/prtea-darwin-amd64.tar.gz"
+      sha256 "7cf73705b1032cc0ce9baeb25fda8474da8f1666fd88927e079355c2256d070f"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/shhac/prtea/releases/download/v0.1.1/prtea-linux-arm64.tar.gz"
-      sha256 "1e8d880c39841abfa849703de6d5c075fe32f717596a620718861bdd7a51bcb0"
+      url "https://github.com/shhac/prtea/releases/download/v0.2.0/prtea-linux-arm64.tar.gz"
+      sha256 "22b7afca42d09105ee807e9bc6d8486d89899df7dd12cf7f47224054976a4f96"
     end
     on_intel do
-      url "https://github.com/shhac/prtea/releases/download/v0.1.1/prtea-linux-amd64.tar.gz"
-      sha256 "afe2a391f6a3397d646bdbc9c35c9e778ef433b0296b027c06e3c98afa90cf5f"
+      url "https://github.com/shhac/prtea/releases/download/v0.2.0/prtea-linux-amd64.tar.gz"
+      sha256 "291fd879b98fb347cb7fd1ba1ea1edca5c5f40a8070b0266ce5055c4bba6327f"
     end
   end
 
@@ -31,6 +31,6 @@ class Prtea < Formula
   end
 
   test do
-    assert_match "0.1.1", shell_output("#{bin}/prtea --version")
+    assert_match "0.2.0", shell_output("#{bin}/prtea --version")
   end
 end
