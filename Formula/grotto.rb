d5ebc2c2bd64e348eebf56823035eb00eb595dcd
Paul Somers <paulsomers@gmail.com>
2026-02-24 16:35:33 +0000

    Update grotto formula to v0.6.1

--- Formula/grotto.rb
+++ Formula/grotto.rb
@@ -1,17 +1,17 @@
 class Grotto < Formula
   desc "A permissive, user-friendly gRPC client"
   homepage "https://github.com/shhac/grotto"
-  version "0.6.0"
+  version "0.6.1"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/shhac/grotto/releases/download/v0.6.0/grotto_0.6.0_darwin_arm64.tar.gz"
-      sha256 "ef88eca3c625ec97b527c93b16f9a6f8a88ab6fc0b54748e6b28fcd351b423e5"
+      url "https://github.com/shhac/grotto/releases/download/v0.6.1/grotto_0.6.1_darwin_arm64.tar.gz"
+      sha256 "48fc2dd2e647dd1bc2a544a1027e75042f00a734593eb2e5a50c4ad36812c7fb"
     end
     on_intel do
-      url "https://github.com/shhac/grotto/releases/download/v0.6.0/grotto_0.6.0_darwin_amd64.tar.gz"
-      sha256 "8ff6680f202cc1850c31d9c08f3b024a4dd419b186583396d62b3e61f7c1f474"
+      url "https://github.com/shhac/grotto/releases/download/v0.6.1/grotto_0.6.1_darwin_amd64.tar.gz"
+      sha256 "4e4d1c6d4d324bac8bfbedb4d91d36b6c29971d218ec492ff063e05ae1ccdd40"
     end
   end
 
