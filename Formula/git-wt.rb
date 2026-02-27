d2bb50927637324880af979189ea433c7d7c6748
Paul Somers <paulsomers@gmail.com>
2026-02-27 17:22:05 +0000

    git-wt 0.6.1

--- Formula/git-wt.rb
+++ Formula/git-wt.rb
@@ -1,28 +1,28 @@
 class GitWt < Formula
   desc "Fast CLI tool for managing git worktrees with enhanced features"
   homepage "https://github.com/shhac/git-wt"
-  version "0.6.0"
+  version "0.6.1"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/shhac/git-wt/releases/download/v0.6.0/git-wt-aarch64-macos.tar.gz"
-      sha256 "831427e8ae5d3fb6616024c0584f7fcf4468424c0e603797c6e093c1a34e3d67"
+      url "https://github.com/shhac/git-wt/releases/download/v0.6.1/git-wt-aarch64-macos.tar.gz"
+      sha256 "05ccf0ad0276285c4bc236b1b5cb9bf94e7ceeaf5d6fe1f9d06c91b3b64c2e8f"
     end
     on_intel do
-      url "https://github.com/shhac/git-wt/releases/download/v0.6.0/git-wt-x86_64-macos.tar.gz"
-      sha256 "ed7922f493e5c7e613f835be0767ac0ad01eba0319694b3cc4d7fd39471829ff"
+      url "https://github.com/shhac/git-wt/releases/download/v0.6.1/git-wt-x86_64-macos.tar.gz"
+      sha256 "a1c7232d987767a67d30ce8114e908baa06c9a906f9e191e4c59bcfedfd8264f"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/shhac/git-wt/releases/download/v0.6.0/git-wt-aarch64-linux.tar.gz"
-      sha256 "9c3a0cc655f3054450067a821faacb259592f5bc1282aa0ba442a8dc7a4f15b7"
+      url "https://github.com/shhac/git-wt/releases/download/v0.6.1/git-wt-aarch64-linux.tar.gz"
+      sha256 "4c5ad5373fa3bcc4a247fe4ecb25049c6cf1602d6111730c2c0a82f206bee49d"
     end
     on_intel do
-      url "https://github.com/shhac/git-wt/releases/download/v0.6.0/git-wt-x86_64-linux.tar.gz"
-      sha256 "d2771f4ab326e7273ae953e78b21e162a5c3fc5062ceae90d8219a949fe50003"
+      url "https://github.com/shhac/git-wt/releases/download/v0.6.1/git-wt-x86_64-linux.tar.gz"
+      sha256 "b463020266fbbed50f05a4fd18a3000897e3f2454cb1a711093bfec97c8073c7"
     end
   end
 
@@ -49,7 +49,7 @@ class GitWt < Formula
   end
 
   test do
-    assert_match "git-wt version 0.6.0", shell_output("#{bin}/git-wt --version")
+    assert_match "git-wt version 0.6.1", shell_output("#{bin}/git-wt --version")
     assert_match "worktree", shell_output("#{bin}/git-wt --help")
   end
 end
