f43d10ba2d34304338a2159aff5a14fcb2a5fe43
Paul Somers <paulsomers@gmail.com>
2026-02-28 19:26:26 +0000

    git-hunk 0.6.0

--- Formula/git-hunk.rb
+++ Formula/git-hunk.rb
@@ -1,28 +1,28 @@
 class GitHunk < Formula
   desc "Non-interactive CLI for staging git diff hunks by content hash"
   homepage "https://github.com/shhac/git-hunk"
-  version "0.5.0"
+  version "0.6.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/shhac/git-hunk/releases/download/v0.5.0/git-hunk-aarch64-macos.tar.gz"
-      sha256 "eece9629e8b75e101fae13ca6e79a4e482ac1ec11e14e37ad5dc60977aefeef6"
+      url "https://github.com/shhac/git-hunk/releases/download/v0.6.0/git-hunk-aarch64-macos.tar.gz"
+      sha256 "1d489bf19bf8a28312352dd3cd802f5e05aa13e22a50c523e9a3176b1569b91b"
     end
     on_intel do
-      url "https://github.com/shhac/git-hunk/releases/download/v0.5.0/git-hunk-x86_64-macos.tar.gz"
-      sha256 "51d6425ab0f29a898f63ca2d773f8637e3614cd4678d4f3bbaf2fc741ce7d6c3"
+      url "https://github.com/shhac/git-hunk/releases/download/v0.6.0/git-hunk-x86_64-macos.tar.gz"
+      sha256 "4772c967f98884c1dc1530cd307d6cb2996e12e649b8488b53ad8b3ac5ead225"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/shhac/git-hunk/releases/download/v0.5.0/git-hunk-aarch64-linux.tar.gz"
-      sha256 "743315dbc73a1cd00f6012da9ca50bc9ba74f00a07730dc41730ab117dd1df78"
+      url "https://github.com/shhac/git-hunk/releases/download/v0.6.0/git-hunk-aarch64-linux.tar.gz"
+      sha256 "54ffd6bd7de36b1bc8e92be06df5031aee8939ae73c21a026aff6e3e5b124e84"
     end
     on_intel do
-      url "https://github.com/shhac/git-hunk/releases/download/v0.5.0/git-hunk-x86_64-linux.tar.gz"
-      sha256 "e56ab1b07e544e5d3082e910920502f59e17f289b731989ad473c77c1cd15c8c"
+      url "https://github.com/shhac/git-hunk/releases/download/v0.6.0/git-hunk-x86_64-linux.tar.gz"
+      sha256 "468073709a1ec9611167a7d0a2e02fa5991792570b66a8013336121c7d961407"
     end
   end
 
@@ -32,6 +32,6 @@ class GitHunk < Formula
   end
 
   test do
-    assert_match "git-hunk 0.5.0", shell_output("#{bin}/git-hunk --version")
+    assert_match "git-hunk 0.6.0", shell_output("#{bin}/git-hunk --version")
   end
 end
