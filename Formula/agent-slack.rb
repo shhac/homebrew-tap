bb7902dc59893df1c96da5ec4adcda5d03d175f2
Paul Somers <paulsomers@gmail.com>
2026-06-13 13:57:11 +0100

    agent-slack 0.5.4: fix tarball member name + shas

--- Formula/agent-slack.rb
+++ Formula/agent-slack.rb
@@ -7,22 +7,22 @@ class AgentSlack < Formula
   on_macos do
     on_arm do
       url "https://github.com/shhac/agent-slack/releases/download/v0.5.4/agent-slack-darwin-arm64.tar.gz"
-      sha256 "76edb582f743158cea25b266e5a1e7ba7c672fefcbcac2fa835574cfe470ed6b"
+      sha256 "742289b94740d857ab4918579a3e9efc3094bf5facf2b4dd48db8a1c0002c756"
     end
     on_intel do
       url "https://github.com/shhac/agent-slack/releases/download/v0.5.4/agent-slack-darwin-amd64.tar.gz"
-      sha256 "89069d388b60b9cac201e984cedf31739d1bbd5de9c96347ff12eaba9a8406ff"
+      sha256 "eebb419677f662fc6d355e00eec3c21cd242cb15fff54c42e397b3422123b5fb"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/shhac/agent-slack/releases/download/v0.5.4/agent-slack-linux-arm64.tar.gz"
-      sha256 "d1c58e7786e19c6d35a2d3e8fb049ac3ea00091cfbff2c2f7c9a554a0f05cc1c"
+      sha256 "922c50e43d56fd86e58c4a65b7d087a6ceea1d571c647a5512babad97d2bf88f"
     end
     on_intel do
       url "https://github.com/shhac/agent-slack/releases/download/v0.5.4/agent-slack-linux-amd64.tar.gz"
-      sha256 "17b19429622cf382beecc8cc1322648f03dfd1357d3e0cae47c21fea14a5a7ef"
+      sha256 "dc10ce2958ce82ec5f291836f6f2618f8db45eb9457ce58d90c8df1b21162ec9"
     end
   end
 
