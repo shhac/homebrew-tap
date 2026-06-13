231778f1653e03194c931f9537d6989188ba933b
Paul Somers <paulsomers@gmail.com>
2026-06-13 13:58:42 +0100

    agent-slack 0.5.4: strip v-prefix from version stamp

--- Formula/agent-slack.rb
+++ Formula/agent-slack.rb
@@ -7,22 +7,22 @@ class AgentSlack < Formula
   on_macos do
     on_arm do
       url "https://github.com/shhac/agent-slack/releases/download/v0.5.4/agent-slack-darwin-arm64.tar.gz"
-      sha256 "742289b94740d857ab4918579a3e9efc3094bf5facf2b4dd48db8a1c0002c756"
+      sha256 "3458aebbf8a289cd5d1472f9007680df33a523d426adc3f41e53016239a5c63d"
     end
     on_intel do
       url "https://github.com/shhac/agent-slack/releases/download/v0.5.4/agent-slack-darwin-amd64.tar.gz"
-      sha256 "eebb419677f662fc6d355e00eec3c21cd242cb15fff54c42e397b3422123b5fb"
+      sha256 "a9f94c0469622939a8aa7a2b5c8e803d67e9b54aabd553bb693df7eab72ac097"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/shhac/agent-slack/releases/download/v0.5.4/agent-slack-linux-arm64.tar.gz"
-      sha256 "922c50e43d56fd86e58c4a65b7d087a6ceea1d571c647a5512babad97d2bf88f"
+      sha256 "8200c9473c3981a981c728ebe566918df4f0d7e2a2976bc9493f582b35ffca61"
     end
     on_intel do
       url "https://github.com/shhac/agent-slack/releases/download/v0.5.4/agent-slack-linux-amd64.tar.gz"
-      sha256 "dc10ce2958ce82ec5f291836f6f2618f8db45eb9457ce58d90c8df1b21162ec9"
+      sha256 "27a47228eaba262b86992e3b60e00ca1ade362b63685e5e1cbcca3b49e75955b"
     end
   end
 
