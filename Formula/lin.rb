76e0586325dbfe5cf99d55b93bde2f7938c4f591
Paul Somers <paulsomers@gmail.com>
2026-06-19 14:33:13 +0100

    lin 0.19.0

--- Formula/lin.rb
+++ Formula/lin.rb
@@ -1,28 +1,28 @@
 class Lin < Formula
   desc "Linear CLI for humans and LLMs"
   homepage "https://github.com/shhac/lin"
-  version "0.18.0"
+  version "0.19.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/shhac/lin/releases/download/v0.18.0/lin-darwin-arm64.tar.gz"
-      sha256 "801c1089609e60b6f039084d02c0bb7dc3f4a486d4000130eb0f84335acd01e4"
+      url "https://github.com/shhac/lin/releases/download/v0.19.0/lin-darwin-arm64.tar.gz"
+      sha256 "3e9b6227df6efed2b59b768a029ed6b6b9dfcf93df83074dc7c23a549a98502a"
     end
     on_intel do
-      url "https://github.com/shhac/lin/releases/download/v0.18.0/lin-darwin-amd64.tar.gz"
-      sha256 "63b0bd086f4cca83f08ba35173045125260163a86f92ed4d6edd598b6b3b8090"
+      url "https://github.com/shhac/lin/releases/download/v0.19.0/lin-darwin-amd64.tar.gz"
+      sha256 "1062ae0c21065983eaa095d7bd39c916cf219e178f90a624c2f04d88d360bb0e"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/shhac/lin/releases/download/v0.18.0/lin-linux-arm64.tar.gz"
-      sha256 "bc3f30801495514f873834dc560acec3375142d088f4e9a89f7ef294869ac142"
+      url "https://github.com/shhac/lin/releases/download/v0.19.0/lin-linux-arm64.tar.gz"
+      sha256 "d65920557e5050384e777ca89625c9840880d3b62d99029db2f76c3873d3f741"
     end
     on_intel do
-      url "https://github.com/shhac/lin/releases/download/v0.18.0/lin-linux-amd64.tar.gz"
-      sha256 "1768e3b60e87bbe5e0046d39b08ce8be2973ab4c6eda4631e8bb1795dd2d472e"
+      url "https://github.com/shhac/lin/releases/download/v0.19.0/lin-linux-amd64.tar.gz"
+      sha256 "ce5687a76d34a5d03c2368a725965681477310109b31c5afbfe8ac36c717d4ab"
     end
   end
 
@@ -31,7 +31,7 @@ class Lin < Formula
   end
 
   test do
-    assert_match "0.18.0", shell_output("#{bin}/lin --version")
+    assert_match "0.19.0", shell_output("#{bin}/lin --version")
     assert_match "Linear CLI", shell_output("#{bin}/lin --help")
   end
 end
